8bd849cd966dd3d954c1bfd1ba5e23c5689d79db
Carlos Alberto Fachini Filho <carlosalbertofachinifilho@gmail.com>
2023-04-04 16:09:38 -0300

    criação das migrations e definiçõa dos models jobs e company

---
+++ database/migrations/2023_04_04_154859_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->enum('remote', ['yes','no']);
+            $table->integer('type');
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('cascade');
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
